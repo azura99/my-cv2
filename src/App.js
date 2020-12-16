25692193ce1cb4c2bda0595b3fce5c200a76ff37
ali <aliasimgg@gmail.com>
2020-12-16 11:46:45 -0500

    added material

--- src/App.js
+++ src/App.js
@@ -1,11 +1,12 @@
 
 import './App.css';
-
+import Radio from './components/radio'
 function App() {
   return (
     <div className="App">
     <h2>My second deployment bc</h2>
     <h3>Please be gentle senpai</h3>
+    <Radio />
     </div>
 
   );
